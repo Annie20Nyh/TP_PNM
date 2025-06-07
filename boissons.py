534cfc7fab6f204b57081161f4a9f3709469c755
annieadela <annieadela.rahariniaina@student.passerellesnumeriques.org>
2025-06-07 09:42:36 +0300

    correction

--- boissons.py
+++ boissons.py
@@ -1,30 +1,42 @@
-def affiche_menu ():
-    
+def afficher_menu ():
     print("que souhaitez_vous boir ?")
-    print(" 1- Cafe ")
-    print(" 2- the ")
+    print(" 1- Café ")
+    print(" 2- thé ")
     print(" 3- Eau chaude ")
-    print(" 4- quitter ")
+    print(" 4- quitter le menu ")
 
+def gerer_choix () :
+      commandes = []
+      while True:
+            afficher_menu()
+            choix = int(input("Entrez votre choix des  menu : "))
+            if choix==1:
+                  print("votre choix  est Cafe")
+                  commandes.append("Café")
+            elif choix==2:       
+                  print ("votre choix est thé")
+                  commandes.append("Thé")
+            elif choix==3:
+                  print ("votre choix est eau chaude")
+                  commandes.append("Eau chaude")
+            elif  choix==4: 
+                  print (" votre choix est de quitez la menu ")
+                  break
+            else :
+                  print("choix invalider")
+            print ("\n votre toutes les commandes sont : ")
 
-def traite_choix (choix) :
-    if   choix == "1" :
-          print("  votre choix  est Cafe ")
-    elif choix == "2" :       
-           print (" votre choix est the ")
-    elif choix == "3" :
-           print (" votre choix est eau chaude ")
-    elif  choix == "4"  : 
-           print (" votre choix est de quitez  ")
-           return "Quitter"
-    else : 
-          print ( " choix invalide")
-          return None
+      if commandes:
+            computeur = {}
+            pluriel={"Café": "cafés", "Thé":"thés", "Eau chaude": "Eau chaudes"}
+            historique={}
+            for boissons in commandes :
+                  computeur[boissons]= computeur.get(boissons, 0) + 1
+            for boissons, nombre in computeur.items( ):
+                    nom_boissons = boissons if nombre==1 else pluriel.get(boissons, "s")
+                    historique[nom_boissons] = nombre
+            print(historique)
+            
 
-
-
-      
-
-           
-
-    
+      else :
+            print("Aucune menu n'a été commandes .")
--- main.py
+++ main.py
@@ -1,20 +1,2 @@
-from boissons import affiche_menu, traite_choix
-
-def main():
-    while True:
-        affiche_menu()
-        choix = input("Entrez votre choix des  menu : ")
-        if choix == "4":
-            traite_choix(choix)
-            break
-        dernier_choix = traite_choix(choix)
-        print()  
-
-        if dernier_choix:
-           print(f"\nRésumé : Votre dernier choix était : {dernier_choix}. Merci et à bientôt !")
-
-if __name__ == "__main__":
-    main()
-
-
-                                                                                      
+from boissons import gerer_choix, afficher_menu
+gerer_choix()
